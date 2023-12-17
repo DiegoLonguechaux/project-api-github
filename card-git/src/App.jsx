989ec7988123241371365920bb2ed0282142e03a
DiegoLonguechaux <diego.longuechaux@gmail.com>
2023-12-17 16:53:36 +0100

    modifs

--- card-git/src/App.jsx
+++ card-git/src/App.jsx
@@ -9,13 +9,13 @@ export default function App() {
   if (data) console.log(data);
 
   return (
-    <div className="flex justify-center items-center">
+    <div className="flex justify-center items-center content-center">
       <div className="flex flex-col gap-6">
         <div className="flex justify-between items-center text-center">
-          <h2>devfinder</h2>
-          <div className="flex items-center gap-3">
-            <p>DARK</p>
-            <svg xmlns="http://www.w3.org/2000/svg" height="16" width="12" viewBox="0 0 384 512"><path d="M223.5 32C100 32 0 132.3 0 256S100 480 223.5 480c60.6 0 115.5-24.2 155.8-63.4c5-4.9 6.3-12.5 3.1-18.7s-10.1-9.7-17-8.5c-9.8 1.7-19.8 2.6-30.1 2.6c-96.9 0-175.5-78.8-175.5-176c0-65.8 36-123.1 89.3-153.3c6.1-3.5 9.2-10.5 7.7-17.3s-7.3-11.9-14.3-12.5c-6.3-.5-12.6-.8-19-.8z"/></svg>
+          <h2 className="font-bold font-mono text-xl">devfinder</h2>
+          <div className="flex items-center gap-3 cursor-pointer">
+            <p className="text-grey font-mono text-lg">DARK</p>
+            <svg xmlns="http://www.w3.org/2000/svg" height="24" width="18" viewBox="0 0 384 512" fill="#4a6a9b"><path d="M223.5 32C100 32 0 132.3 0 256S100 480 223.5 480c60.6 0 115.5-24.2 155.8-63.4c5-4.9 6.3-12.5 3.1-18.7s-10.1-9.7-17-8.5c-9.8 1.7-19.8 2.6-30.1 2.6c-96.9 0-175.5-78.8-175.5-176c0-65.8 36-123.1 89.3-153.3c6.1-3.5 9.2-10.5 7.7-17.3s-7.3-11.9-14.3-12.5c-6.3-.5-12.6-.8-19-.8z"/></svg>
           </div>
         </div>
         <Form setUser={setUser} />
--- card-git/src/components/Form.jsx
+++ card-git/src/components/Form.jsx
@@ -5,21 +5,21 @@ export default function Form({ setUser }) {
             setUser(e.target.elements.user.value);
             e.target.user.value = "";
         }}
-        className="flex flex-row gap-4 items-center justify-between bg-white shadow-2xl rounded-lg p-2"
+        className="font-mono flex flex-row gap-4 items-center justify-between bg-card shadow-2xl rounded-lg p-2"
         >
                 <div className="flex grow gap-4">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="#0277ff" class="w-6 h-6">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
                     </svg>
                     <input
                         type="text"
                         name="user"
-                        className="w-full bg-white"
+                        className="w-full bg-card"
                         placeholder="Search Github username..."
                         onFocus={(e) => (e.target.value = "")}
                     />
                 </div>
-                <button type="submit" className="bg-blue-600 rounded-lg p-2 text-white">Search</button>
+                <button type="submit" className="bg-primary rounded-lg p-2 text-card">Search</button>
     
         </form>
     )
