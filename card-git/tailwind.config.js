9a08693b011dca247afa1cd549056420d63b6dfe
DiegoLonguechaux <diego.longuechaux@gmail.com>
2024-01-09 10:25:10 +0100

    maj

--- card-git/tailwind.config.js
+++ card-git/tailwind.config.js
@@ -5,14 +5,6 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    // colors:{
-    //   'background': '#f5f7ff',
-    //   'card': '#ffffff',
-    //   'primary': '#0277ff',
-    //   'lightgrey': '#91a5d4',
-    //   'grey': '#4a6a9b',
-    //   'dark':' #000000',
-    // },
     fontFamily: {
       mono: ['"PT Mono"', 'monospace'],
     },
